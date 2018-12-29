b8fc57e0d08f000801de67d17c697ef964a4c335
Gbenga Sam <adeyeye546@gmail.com>
2018-12-29 12:04:13 +0100

    Creating servers

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-var fs = require('fs');
+var http = require('http');
 
-fs.unlink('writeme.txt');
+var server = http.createServer(function(req, res){
+  console.log('request was made: ' + req.url);
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hey buddies');
+});
+
+server.listen(3000, '127.0.0.1');
+console.log('Now listening to port 3000');
