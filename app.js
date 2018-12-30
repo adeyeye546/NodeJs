a40a46069b4c46d185c0faff2eabd45c4d6aff78
Gbenga Sam <adeyeye546@gmail.com>
2018-12-30 19:15:00 +0100

    Serving html pages

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var http = require('http');
 
+var fs  = require('fs');
+
+
 var server = http.createServer(function(req, res){
   console.log('request was made: ' + req.url);
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hey buddies');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var myReadStream = fs.createReadStream(__dirname + '/index.html','utf8');
+  var myWriteStream = fs.createWriteStream(__dirname + '/writeme.txt')
+
+  myReadStream.pipe(res);
 });
 
 server.listen(3000, '127.0.0.1');
